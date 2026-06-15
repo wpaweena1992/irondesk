16dcea5ecc0c0383d308d50ee6621a9e55d46059
Paweena.w <paweena.w@thaixon.co.th>
2026-06-15 11:33:47 +0700

    fix: Database can not connect

--- server/db.js
+++ server/db.js
@@ -4,13 +4,13 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // 🔥 ใส่เฉพาะใน db.js เพื่อให้เชื่อมต่อ Neon DB บน Render ได้
+    rejectUnauthorized: false // 🔥 เปิด SSL สำหรับต่อฐานข้อมูล Neon DB บน Cloud
   }
 });
 
 module.exports = {
   query: async (text, params) => {
     const res = await pool.query(text, params);
-    return res.rows; // ส่งค่ากลับเป็น Array เพื่อให้ index.js นำไปใช้งานต่อได้ทันที
+    return res.rows; // คืนค่าเป็น Array เพื่อให้แมตช์กับ [tm], [am] ใน index.js ของคุณครับ
   }
 };
